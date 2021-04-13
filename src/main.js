6f66c0df2101389dfc6bbeb0372d240dcda30124
Minej <minej.pak@academiavss.si>
2021-04-13 12:34:00 +0200

    Popravek

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
+import App from './App.vue'
 import router from '@/router.js'
 import '@/assets/globalni.css'
 
 const moja = createApp(App);
 moja.use(router);
 moja.mount('#app')
-
