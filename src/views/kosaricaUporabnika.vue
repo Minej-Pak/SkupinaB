85ba66f5a2ce390571d067dd68bfa71a37895fc2
VidW <vid.weilguny@academiavss.si>
2021-04-27 13:04:04 +0200

    Deluje

--- src/views/kosaricaUporabnika.vue
+++ src/views/kosaricaUporabnika.vue
@@ -2,8 +2,8 @@
     <h1>Zgodovina kosarice</h1>
   <input type="text" v-model="id" placeholder="Vnesi id naročila">
   <button type="submit" @click="handleButton">Izbriši naročilo</button>
-     <ul v-if="prikazano.username = vpisanUporabnik.username">
-       <li v-for="items in prikazano" :key="items">{{items.narocilo}} {{items.prikazanId}} {{items.username}}<br>{{items.name}} {{items.price}}</li>
+     <ul>
+       <li v-for="items in prikazano" :key="items">{{items.username}}{{items.narocilo}} {{items.prikazanId}}<br>{{items.name}}</li>
      </ul>
 </template>
 
@@ -18,16 +18,22 @@ export default {
   },
   methods: {
     handleButton() {
+      let index;
+      let kolikoIzbrisat = 0;
       const id = this.id
       for (let i = 0; i <= this.prikazano.length - 1; i++) {
-          for (let y = 0; y <= this.prikazano.length - 1; y++) {
-            if (this.prikazano[i].id == id) {
-            this.prikazano.splice(i, y);
-            // localStorage.setItem('data', JSON.stringify(this.prikazano));
-            console.log(this.prikazano)
-          }
+          if (this.prikazano[i].id == id) {
+            kolikoIzbrisat = kolikoIzbrisat+1;
         }
       }
+      for (let i = 0; i <= this.prikazano.length - 1; i++) {
+        if (this.prikazano[i].id == id) {
+          index = i;
+          break;
+        }
+      }
+      this.prikazano.splice(index ,kolikoIzbrisat);
+      //localStorage.setItem('uporabnikZgodovina', JSON.stringify(this.prikazano));
     }
   }
 }
